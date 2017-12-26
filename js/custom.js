caee8f9f06d92dfb1759b71fe349caeddd9ef934
Webchirpy <hello@webchirpy.com>
2017-12-26 16:41:18 +0530

    Iniitial Website design commit - Webchirpy

---
+++ js/custom.js
@@ -0,0 +1,45 @@
+(function ($) {
+
+	new WOW().init();
+
+	jQuery(window).load(function() {
+		jQuery("#preloader").delay(100).fadeOut("slow");
+		jQuery("#load").delay(100).fadeOut("slow");
+	});
+
+
+	//jQuery to collapse the navbar on scroll
+	$(window).scroll(function() {
+		if ($(".navbar").offset().top > 50) {
+			$(".navbar-fixed-top").addClass("top-nav-collapse");
+		} else {
+			$(".navbar-fixed-top").removeClass("top-nav-collapse");
+		}
+	});
+
+	//jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+
+		$('.navbar-nav li a').on('click', function(event) {
+
+			if( $(this).is('a:not([href^="#"])') || $(this).attr("href") == '#' ) {
+	     return;
+	    }
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+
+		$('.page-scroll a').on('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+
+	});
+
+})(jQuery);
