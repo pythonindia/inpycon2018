6ac293dc9ca72d5b751b78b5b2bdcd2af20b6dca
Bharat Saraswat <bsaraswat45@gmail.com>
2018-06-01 04:03:08 +0530

    removed log, and added indentation

--- js/custom.js
+++ js/custom.js
@@ -20,15 +20,14 @@
 			$("#logo").css("display","none");
 			$(".navbar-fixed-top").removeClass("top-nav-collapse");
 		}
-		console.log('srolling');
 
 		// share-bar-ribbon class toggle
 		if($(".share-bar-ribbon").offset().top > $("#venue").offset().top - $(".share-bar-ribbon").outerHeight()){
-    		$(".share-bar-ribbon").removeClass('share-bar-ribbon--dark').addClass('share-bar-ribbon--light');
-    	}
-    	else{
-    		$(".share-bar-ribbon").removeClass('share-bar-ribbon--light').addClass('share-bar-ribbon--dark');
-    	}
+    			$(".share-bar-ribbon").removeClass('share-bar-ribbon--dark').addClass('share-bar-ribbon--light');
+    		}
+    		else{
+    		 	$(".share-bar-ribbon").removeClass('share-bar-ribbon--light').addClass('share-bar-ribbon--dark');
+    		}
 
 
 	});
