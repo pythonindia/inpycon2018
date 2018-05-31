8ff72beabd6acd89f02ecb99022439e40ce225de
Bharat Saraswat <bsaraswat@gmail.com>
2018-06-01 03:42:38 +0530

    added class toggle on scroll for ligh tand dark theme

--- js/custom.js
+++ js/custom.js
@@ -20,6 +20,17 @@
 			$("#logo").css("display","none");
 			$(".navbar-fixed-top").removeClass("top-nav-collapse");
 		}
+		console.log('srolling');
+
+		// share-bar-ribbon class toggle
+		if($(".share-bar-ribbon").offset().top > $("#venue").offset().top - $(".share-bar-ribbon").outerHeight()){
+    		$(".share-bar-ribbon").removeClass('share-bar-ribbon--dark').addClass('share-bar-ribbon--light');
+    	}
+    	else{
+    		$(".share-bar-ribbon").removeClass('share-bar-ribbon--light').addClass('share-bar-ribbon--dark');
+    	}
+
+
 	});
 
 	//jQuery for page scrolling feature - requires jQuery Easing plugin
