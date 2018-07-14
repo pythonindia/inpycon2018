73f9bfc75533177eaacc1ac3721c3cce73530f85
Ananyo Maiti <ananyoevo@gmail.com>
2018-07-14 10:43:51 +0530

    Add platinum sponsor

--- js/sponsor.js
+++ js/sponsor.js
@@ -11,9 +11,10 @@ constructSponsorBlock = function(response){
         $.each(response,function(key,value){
             var title=  "<div class='row text-center'><h3>" + key +" Sponsors</h3>";
             $.each(value,function(index,item) {
-                var str = "<div class='col-md-4'><img src='img/border2.png' class='border2 border2a' alt='"+item["alt-text"]+"'>\
-                <img src='"+ item.img +"' alt='"+item["alt-text"]+"' class='img-responsive sponsora center-block'></div>";
-                title +=str;
+                var str = "<div class='col-md-offset-4 col-md-4'><a href='" + item["link"] +
+                "' target='_blank'><img src='" + item["img"] + "' alt='" + item["alt-text"] +
+                "' class='img-responsive sponsor center-block'></a></div>";
+                title += str;
                 str = "";
             });
             text += title + "</div><br /><br /><br /><br />";
