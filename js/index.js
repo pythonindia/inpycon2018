49112b63198d52383114cf7c42334c0dd20bf87e
Ananyo Maiti <ananyoEVO@gmail.com>
2018-09-09 13:27:20 +0530

    Organise the Navbar - Add dropdowns for more options (#183)
    * Optimising the navbar
    
    * Organising Navbar according to #149
    
    * Redirect bug
    
    * Adding caret and including View Proposal Button
    
    * Implemented but css is siuue
    
    * Css fix
    
    * Navbar center align
    
    * Mobile page logo
    
    * Carat position
    
    * Added @bhansa uggestion and now it is working fine :'), thanks
    
    * Fixed according to what @bhansa said
    
    * Making it according to comments
    
    * Made changes
    
    * Adding schedule and fixed home issue
    
    * Removed on active blue color and reverted #schedule  to #
    
    * Done @ananyo2012
    
    * Fix things in navbar

--- js/index.js
+++ js/index.js
@@ -2,3 +2,16 @@ $(document).ready(function () {
     $('div#banner-text > h1').delay(400).fadeIn(1000);
     $('div#banner-text > h2').delay(1400).fadeIn(1000);
 });
+
+$(window).scroll(function() {
+	if ($(".navbar").offset().top > 50) {
+		$(".navbar .container").css("width","1170px");
+		$(".index-logo").css("display","inline-block");
+		$(".index-logo").css("float","left");
+		$(".navbar-fixed-top").addClass("top-nav-collapse");
+	} else {
+		$(".navbar .container").css("width","970px");
+		$(".index-logo").css("display","none");
+		$(".navbar-fixed-top").removeClass("top-nav-collapse");
+	}
+});
