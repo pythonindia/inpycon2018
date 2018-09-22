9386c01fb5a40de073ef79e084d4e1a1a4ea0286
Ananyo Maiti <ananyoevo@gmail.com>
2018-09-22 23:43:17 +0530

    Update sponsors

--- js/sponsor.js
+++ js/sponsor.js
@@ -10,10 +10,11 @@ constructSponsorBlock = function(data){
 
         $.each(data, function(key, value) {
             sponsorContent += "<h3 class='text-center'>" + key + "</h3>";
-            for(var i = 0; i < value.length; i += 3) {
-                sponsorGroups = value.slice(i, i+3);
+            var cellsEachRow = (key.toLowerCase() == "platinum" ? 4 : 3)
+            for(var i = 0; i < value.length; i += cellsEachRow) {
+                sponsorGroups = value.slice(i, i+cellsEachRow);
                 sponsorContent += "<div class='row'>"
-                sponsorContent += buildSponsorRow(sponsorGroups);
+                sponsorContent += buildSponsorRow(sponsorGroups, key);
                 sponsorContent += "</div>";
             }
 
@@ -22,7 +23,7 @@ constructSponsorBlock = function(data){
         $("#sponsors-content").append(sponsorContent);
 }
 
-buildSponsorRow = function(items) {
+buildSponsorRow = function(items, category) {
     var totalItems = items.length;
     var offset = '';
     var rowContent = '';
@@ -38,7 +39,7 @@ buildSponsorRow = function(items) {
     }
 
     for (var i = 0; i < totalItems; i++) {
-        rowContent += "<div class='" + (i == 0 ? offset : "")  + "col-md-4 sponsor-padding'><a href='" + items[i]["link"] +
+        rowContent += "<div class='" + (i == 0 ? offset : "")  + (category.toLowerCase() == "platinum" ? "col-md-3" : "col-md-4") + " sponsor-padding'><a href='" + items[i]["link"] +
             "' target='_blank'><div class='img-container'><img src='" + items[i]["img"] + "' alt='" + items[i]["alt-text"] +
             "' class='img-responsive sponsor center-block " + items[i]["class"] +"'></a></div></div>";
     }
