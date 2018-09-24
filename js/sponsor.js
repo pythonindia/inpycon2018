8b8d1d870878b42194ba8ba16889925b613601d3
akshat0047 <pande.akshat21@gmail.com>
2018-09-25 01:46:57 +0530

    added sponsor popups and company descriptions

--- js/sponsor.js
+++ js/sponsor.js
@@ -1,10 +1,11 @@
-$(document).ready(function(){
 
-    $.get('./data/sponsors.json',function(response){
+$(document).ready(function(){
+    $.getJSON('./data/sponsors.json',function(response){
         constructSponsorBlock(response);
+        createPopupAnimation();
     });
 });
-
+var popupId=1;
 constructSponsorBlock = function(data){
         var sponsorContent = "";
 
@@ -23,26 +24,43 @@ constructSponsorBlock = function(data){
         $("#sponsors-content").append(sponsorContent);
 }
 
-buildSponsorRow = function(items, category) {
+buildSponsorRow = function(items,category) {
     var totalItems = items.length;
     var offset = '';
     var rowContent = '';
     switch(totalItems) {
         case 1:
-            offset = 'col-md-offset-4 ';
+            offset = 'col-lg-offset-4 col-md-offset-4 ';
             break;
         case 2:
-            offset = 'col-md-offset-2 ';
+            offset = 'col-lg-offset-2 col-md-offset-2 ';
             break;
         default:
             offset = ''
     }
 
     for (var i = 0; i < totalItems; i++) {
-        rowContent += "<div class='" + (i == 0 ? offset : "")  + (["platinum"].indexOf(category.toLowerCase()) >= 0 ? "col-md-3" : "col-md-4") + " sponsor-padding'><a href='" + items[i]["link"] +
-            "' target='_blank'><div class='img-container'><img src='" + items[i]["img"] + "' alt='" + items[i]["alt-text"] +
-            "' class='img-responsive sponsor center-block " + items[i]["class"] +"'></a></div></div>";
+      rowContent += "<div class='" + (i == 0 ? offset : "")  + (["platinum"].indexOf(category.toLowerCase()) >= 0 ? "col-md-3" : "col-md-4") + " sponsor-padding'>"+
+             "<div id='"+popupId+"' class='img-container link'><img src='" + items[i]["img"] + "' alt='" + items[i]["alt-text"] +
+          "' class='img-responsive sponsor center-block " + items[i]["class"] +"'></div>"+
+            "<div class='popup text-center'><div class='row'><div class='col-xs-12 col-sm-12 col-md-12 col-lg-12'>"+
+               "<span class='close'><img src='icons/window-close-solid.svg' class='fas fa-window-close close-sponsor-dialogue '></span><h4 class='pop-head'>"+items[i]["alt-text"]+"</h4><p class='pop-data'>"+items[i]["description"]+
+               "</p><a href='"+items[i]["link"]+"' target='_blank' title='"+items[i]["alt-text"]+"' class='pop-head btn btn-details text-center'>Website</a>"+
+           "</div></div></div></div>";
+            popupId=popupId+1;
     }
-
     return rowContent;
+}
+
+function createPopupAnimation(){
+      var id;
+      $(".link").click(function(event){
+           $(".popup").css("display","none");
+           var id = $(this).attr("id");
+           $(".popup").eq(id-1).css("display","block");
+      });
+      $(".close").click(function(event){
+           $(".popup").css("display","none");
+           event.stopPropagation();
+      });
 }
